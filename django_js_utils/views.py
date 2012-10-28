d634a0a75d0b34a63be224bf4eb521553bdfdaab
Jacob Magnusson <m@jacobian.se>
2012-10-29 00:57:41 +0100

    Namespace keys in the javascript output like Django does. Also added new setting JS_UTILS_ALLOWED_NAMESPACES so one can specify which namespaces urls to generate

--- django_js_utils/views.py
+++ django_js_utils/views.py
@@ -1,18 +1,23 @@
 import re
 import sys
 import types
-from django.utils.datastructures import SortedDict
-from django.conf import settings
-from django.http import HttpResponse
 import django.utils.simplejson as json
+from django.conf import settings
 from django.core.urlresolvers import RegexURLPattern, RegexURLResolver
+from django.http import HttpResponse
+from django.utils.datastructures import SortedDict
+
+ALLOWED_NAMESPACES = getattr(settings, 'JS_UTILS_ALLOWED_NAMESPACES', None)
+
 
 def jsurls(request):
 
-    RE_KWARG = re.compile(r"(\(\?P\<(.*?)\>.*?\))") #Pattern for recongnizing named parameters in urls
-    RE_ARG = re.compile(r"(\(.*?\))") #Pattern for recognizing unnamed url parameters
+    # Pattern for recognizing named parameters in urls
+    RE_KWARG = re.compile(r"(\(\?P\<(.*?)\>.*?\))")
+    # Pattern for recognizing unnamed url parameters
+    RE_ARG = re.compile(r"(\(.*?\))")
 
-    def handle_url_module(js_patterns, module_name, prefix=""):
+    def handle_url_module(js_patterns, module_name, prefix="", namespace=""):
         """
         Load the module and output all of the patterns
         Recurse on the included modules
@@ -31,24 +36,34 @@ def jsurls(request):
         for pattern in patterns:
             if issubclass(pattern.__class__, RegexURLPattern):
                 if pattern.name:
+                    pattern_name = u':'.join((namespace, pattern.name)) if \
+                                                    namespace else pattern.name
                     full_url = prefix + pattern.regex.pattern
-                    for chr in ["^","$"]:
+                    for chr in ["^", "$"]:
                         full_url = full_url.replace(chr, "")
-                    #handle kwargs, args
+                    # Handle kwargs, args
                     kwarg_matches = RE_KWARG.findall(full_url)
                     if kwarg_matches:
                         for el in kwarg_matches:
-                            #prepare the output for JS resolver
+                            # Prepare the output for JS resolver
                             full_url = full_url.replace(el[0], "<%s>" % el[1])
-                    #after processing all kwargs try args
+                    # After processing all kwargs try args
                     args_matches = RE_ARG.findall(full_url)
                     if args_matches:
                         for el in args_matches:
-                            full_url = full_url.replace(el, "<>")#replace by a empty parameter name
-                    js_patterns[pattern.name] = "/" + full_url
+                            # Replace by a empty parameter name
+                            full_url = full_url.replace(el, "<>")
+                    js_patterns[pattern_name] = "/" + full_url
             elif issubclass(pattern.__class__, RegexURLResolver):
+                if ALLOWED_NAMESPACES is None:
+                    pass
+                elif pattern.namespace not in ALLOWED_NAMESPACES:
+                    continue
+
                 if pattern.urlconf_name:
-                    handle_url_module(js_patterns, pattern.urlconf_name, prefix=pattern.regex.pattern)
+                    handle_url_module(js_patterns, pattern.urlconf_name,
+                                      prefix=pattern.regex.pattern,
+                                      namespace=pattern.namespace)
 
     js_patterns = SortedDict()
     handle_url_module(js_patterns, settings.ROOT_URLCONF)
@@ -56,7 +71,6 @@ def jsurls(request):
     from django.template.loader import get_template
 
     response = HttpResponse(mimetype='text/javascript')
-    response.write('django_js_utils_urlconf = ');
+    response.write('django_js_utils_urlconf = ')
     json.dump(js_patterns, response)
     return response
-
